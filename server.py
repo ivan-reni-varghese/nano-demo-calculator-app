a298e10efa0b49afb76a65c63e8d9ef90c149342
Ivan Reni Varghese <ivan.reni.varghese@gmail.com>
2023-08-06 19:59:01 +0530

    success commit

--- server.py
+++ server.py
@@ -1,19 +1,28 @@
-from flask import Flask
+from flask import Flask, Response,request
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello world!',200
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    data=request.json
+    n1=int(data["first"])
+    n2=int(data["second"])
+    result=n1+n2
+    return {"result":result},200
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    data=request.json
+    n1=int(data["first"])
+    n2=int(data["second"])
+    result=n1-n2
+    return {"result":result},200,{"Content-Type":"application/json"}
+    
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
